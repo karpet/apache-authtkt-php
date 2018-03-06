4fe2004e6472c6573f01c0800394577e50381cfe
Peter Karman <karpet@peknet.com>
2018-03-06 12:52:45 -0600

    Must pass "user" param in tests

--- lib/Apache_AuthTkt.php
+++ lib/Apache_AuthTkt.php
@@ -133,8 +133,6 @@ class Apache_AuthTkt {
         // Parse ticket
         $info = $this->parse_ticket($tkt);
 
-        error_log("info:" . var_export($info, true));
-
         // Validate digest
         $expected_digest = $this->get_digest(
             $info['ts'], $ip_address, $info['uid'],
@@ -190,7 +188,6 @@ class Apache_AuthTkt {
         $digest_type = $this->get_digest_type();
         $digest_length = $this->digest_lengths[$digest_type];
         $pattern = '/^(.{' . $digest_length . '})(.{8})(.+?)!(.*)$/';
-        error_log("digest_type:$digest_type digest_length:$digest_length pattern:$pattern");
         if (!preg_match($pattern, $raw, $matches)) {
             $this->set_err("No regex match for '$raw'");
             return null;
@@ -246,8 +243,6 @@ class Apache_AuthTkt {
         $raw = $ipts . $this->get_secret() . $uid . "\0" . $tokens . "\0" . $data;
         $digest_type = $this->get_digest_type();
         $digest0 = hash($digest_type, $raw);
-        error_log(var_export($raw, true));
-        error_log("digest_type:$digest_type digest0:$digest0");
         $digest  = hash($digest_type, $digest0 . $this->get_secret());
         return $digest;
     }
--- test/001-hash.t
+++ test/001-hash.t
@@ -3,7 +3,7 @@
 
 require_once 'TestMore.php';
 
-plan(12);
+plan(16);
 
 require_once 'lib/Apache_AuthTkt.php';
 
@@ -11,12 +11,11 @@ $digests = array('md5', 'sha1', 'SHA256', 'sHa512');
 $last_tkt = '';
 foreach ($digests as $digest) {
     ok( $auth_tkt =new Apache_AuthTkt(array('secret' => 'sekrit', 'digest_type' => $digest)), "new $digest AuthTkt" );
-    ok( $tkt = $auth_tkt->create_ticket(array('uid' => 'user')), "new tkt" );
+    ok( $tkt = $auth_tkt->create_ticket(array('user' => 'username')), "new tkt" );
     cmp_ok( strlen($tkt), '>', strlen($last_tkt), "each digest makes longer ticket" );
     $last_tkt = $tkt;
 
     if (!ok( $auth_tkt->validate_ticket($tkt), "validate ticket $digest" )) {
       diag( $auth_tkt->get_err() );
     }
-    diag_dump($auth_tkt);
 }
